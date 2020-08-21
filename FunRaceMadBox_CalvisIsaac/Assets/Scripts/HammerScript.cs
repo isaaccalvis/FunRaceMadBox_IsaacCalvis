afea8a45d9a569c27b46afe167d285f944c60af1
isaac calvis <isaaccalvis@gmail.com>
2020-08-21 16:07:11 +0200

    Adding rotative hammer

---
+++ FunRaceMadBox_CalvisIsaac/Assets/Scripts/HammerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerScript : MonoBehaviour
+{
+    public float speed = 1f;
+    public float force = 1f;
+    
+    void FixedUpdate()
+    {
+        //GetComponent<Rigidbody>().angularDrag = 0.5f;
+        GetComponent<Rigidbody>().maxAngularVelocity = speed;
+        GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, 1) * force);
+        //transform.Rotate(new Vector3(0,0,1) * speed);
+    }
+}
