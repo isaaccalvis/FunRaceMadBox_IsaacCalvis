a4a0061fa002cfc7d19e3258ff63766b8e1ecf8b
isaac calvis <isaaccalvis@gmail.com>
2020-08-21 17:42:02 +0200

    Improving hammer collision

--- FunRaceMadBox_CalvisIsaac/Assets/Scripts/PlayerBasic.cs
+++ FunRaceMadBox_CalvisIsaac/Assets/Scripts/PlayerBasic.cs
@@ -7,9 +7,14 @@ using UnityEngine.SceneManagement;
 public class PlayerBasic : MonoBehaviour
 {
     // PUBLIC
+    // Player
     public float speed = 1.0f;
     public bool canMove = false;
+    // Ui
     public Text scoreText;
+    // Collision
+    public float y_coll_mod = 1.5f;
+    public float x_coll_mod = 1.5f;
 
     // PRIVATE
     private int score = 0;
@@ -51,6 +56,19 @@ public class PlayerBasic : MonoBehaviour
         if (Input.GetKey("d"))
         {
             GetComponent<Rigidbody>().AddForce(new Vector3(1f, 0f, 0f) * speed);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Vector3 forceDirection = (transform.position - collision.gameObject.transform.position).normalized
+                * collision.transform.parent.parent.GetComponent<HammerScript>().force;
+            forceDirection.y = Mathf.Abs(forceDirection.y) * y_coll_mod;
+            forceDirection.x = Mathf.Abs(forceDirection.x) * x_coll_mod;
+
+            GetComponent<Rigidbody>().AddForce(forceDirection);
         }
     }
 
